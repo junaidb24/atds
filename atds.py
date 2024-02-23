f232c46e0ee5cdfe411dbe7d40eb244e6b045e15
Junaid Bhatti <jbhatti24@students.polytechnic.org>
2024-02-23 12:20:40 -0800

    Added Deque class

--- atds.py
+++ atds.py
@@ -73,3 +73,51 @@ class Queue:
     def __repr__(self):
         return str(self.que)
         
+class Deque:
+    def __init__(self):
+        self.deque = []
+    
+    def add_front(self, item):
+        self.deque.insert(0, item)
+
+    def add_rear(self, item):
+        self.deque.append(item)
+
+    def remove_front(self):
+        if self.is_empty() == False:
+            return self.deque.pop(0)
+        else:
+            return None
+
+    def remove_rear(self):
+        if self.is_empty() == False:
+            return self.deque.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.deque)
+
+    def is_empty(self):
+        if len(self.deque) == 0:
+            return True
+        else:
+            return False
+
+
+def main():
+    # The program goes here
+    queue = Queue()
+    queue.enqueue("Nick")
+    queue.enqueue("Roy")
+    queue.enqueue("Patrick")
+    print(queue.peek())
+    print(queue.size())
+    print(queue.dequeue())
+    print(queue.peek())
+    print(queue.is_empty())
+
+
+if __name__ == "__main__":
+    main()
+
